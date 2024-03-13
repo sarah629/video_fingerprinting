3d023b0d2f219615711a0d839732839ca128881d
0had0 <hadihoussainy75@gmail.com>
2024-03-13 11:59:01 +0100

    Create & Setup the Fingerprints Database

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 SHELL := /bin/bash
 VIDEO_DIR := ./data
 OUTPUT_DIR := ./fingerprints
+DB_DIR := ./database
 
 .PHONY: all
-all: init generate_fingerprints
+all: init setup_database
 
 .PHONY: init
 init:
@@ -12,8 +13,20 @@ init:
 	poetry install
 	@mkdir -p $(OUTPUT_DIR)
 
+.PHONY: create_database
+create_database:
+	@echo "Creating Database in $(DB_DIR)..."
+	poetry run python ./src/create_database.py $(DB_DIR)
+
+.PHONY: setup_database
+setup_database: generate_fingerprints create_database
+	@echo "Setting Up the Database in $(DB_DIR)..."
+	poetry run python ./src/setup_database.py $(VIDEO_DIR) $(DB_DIR)
+
 .PHONY: generate_fingerprints
 generate_fingerprints:
+	@echo "Cleaning previous fingerprints.."
+	@rm -rf $(OUTPUT_DIR)/*
 	@echo "Generating fingerprints for all videos in $(VIDEO_DIR)..."
 	@find $(VIDEO_DIR) -type f \( -iname "*.mp4" -o -iname "*.avi" -o -iname "*.mov" -o -iname "*.mkv" \) -print0 | while IFS= read -r -d $$'\0' video; do \
 		echo "Processing $$video..."; \
---
+++ src/setup_database.py
@@ -0,0 +1,67 @@
+import os
+import pickle
+import sqlite3
+import sys
+from tqdm import tqdm
+from config import Config
+
+
+def load_and_store(folder_path, conn):
+    cur = conn.cursor()
+
+    # Create table if it doesn't exist (adjust column names if needed)
+    cur.execute(
+        """
+      CREATE TABLE IF NOT EXISTS data (
+          video_name TEXT,
+          id INTEGER,
+          data BLOB
+      );
+      """
+    )
+
+    videos = os.listdir(folder_path)
+
+    for video_idx in tqdm(range(len(videos))):
+        video_name = videos[video_idx]
+        video_path = os.path.join(folder_path, video_name)
+        if os.path.isdir(video_path):
+
+            fingerprints = os.listdir(video_path)
+
+            for fingerprint_idx in tqdm(range(len(fingerprints))):
+                filename = fingerprints[fingerprint_idx]
+
+                if filename.endswith(".pkl"):
+                    full_path = os.path.join(video_path, filename)
+                    with open(full_path, "rb") as f:
+                        data = pickle.load(f)
+
+                    file_id = int(filename.split(".")[0])
+
+                    data_bytes = pickle.dumps(data, protocol=pickle.HIGHEST_PROTOCOL)
+
+                    cur.execute(
+                        "INSERT INTO data (video_name, id, data) VALUES (?, ?, ?)",
+                        (video_name, file_id, data_bytes),
+                    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    folder_path = sys.argv[1]
+    db_path = sys.argv[2]
+
+    conn = sqlite3.connect(f"{db_path}/{Config.DB}")
+    print("connection successful ", conn)
+
+    load_and_store(folder_path, conn)
+
+    # cur.execute("SELECT * FROM data LIMIT 10")
+
+    # rows = cur.fetchall()
+
+    # print(pickle.loads(rows[2][2]))
