09c5ac6797013437b77522f3d1952b50e1a05484
0had0 <hadihoussainy75@gmail.com>
2024-03-13 11:59:50 +0100

    Update the matching so it can be done over the previously created database

--- src/matching.py
+++ src/matching.py
@@ -1,23 +1,72 @@
 from scipy.spatial import distance
 import pickle
 import numpy as np
+import sqlite3
+import sys
+from config import Config
+from generation import generate_fingerprints_from_video
 
 
 def load_fingerprint(file_path: str) -> np.ndarray:
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         fingerprint = pickle.load(file)
     return fingerprint
 
 
-def match_fingerprints(query_fingerprint: np.ndarray, candidate_fingerprint: np.ndarray) -> float:
+def match_fingerprints(
+    query_fingerprint: np.ndarray, candidate_fingerprint: np.ndarray
+) -> float:
     return distance.euclidean(query_fingerprint, candidate_fingerprint)
 
 
-if __name__ == '__main__':
-    query_video_fingerprint_path = '../fingerprints/inputs/0.pkl'
-    candidate_video_fingerprint_path = '../fingerprints/inputs/0.pkl'
+scores = {}
 
-    query_fingerprint = load_fingerprint(query_video_fingerprint_path)
-    candidate_fingerprint = load_fingerprint(candidate_video_fingerprint_path)
-    match_score = match_fingerprints(query_fingerprint, candidate_fingerprint)
-    print(f"Match score: {match_score}")
+
+def query_with_logic(conn, input_fingerprints, logic_func):
+    cur = conn.cursor()
+
+    M = len(input_fingerprints)
+    i = 0
+
+    while True:
+        query = f"SELECT * FROM data LIMIT {M} OFFSET {M+i}"
+
+        i += 1
+
+        cur.execute(query)
+
+        rows = cur.fetchall()
+
+        if not rows:
+            break
+
+        logic_func(input_fingerprints, rows)
+
+    conn.close()
+
+    print({[key]: np.mean(value) for key, value in scores})
+
+
+def compute_scores(input_fingerprints, query):
+    for q, f in zip(query, input_fingerprints):
+        video_name, _, fingerprint = q
+
+        score = match_fingerprints(fingerprint, f)
+
+        if scores[video_name]:
+            scores[video_name].append(score)
+        else:
+            scores[video_name] = [score]
+
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    db_path = sys.argv[2]
+
+    input_fingerprint = generate_fingerprints_from_video(
+        input_path, M=Config.M, N=Config.N, H=Config.H, W=Config.W, FPS=Config.FPS
+    )
+
+    conn = sqlite3.connect(f"{db_path}/{Config.DB}")
+
+    query_with_logic(conn, input_fingerprint, compute_scores)
